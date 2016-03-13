c478c53a7c5837acbbcd52242039fc269f798e26
Igor <OldPal@users.noreply.github.com>
2016-03-13 14:17:36 +0200

    Added files via upload

---
+++ Flower_bouquet.java
@@ -0,0 +1,48 @@
+import java.util.List;
+
+
+public class Flower_bouquet {
+    private List<Aster> Asters;
+    private List<Chamomile> Chamomiles;
+    private List<Rose> Roses;
+    private List<Tulip> Tulips;
+    public List<Aster> getAsters() {
+        return Asters;
+}
+
+public void setAsters(List<Aster> Asters) {
+    this.Asters=Asters;
+}
+
+    public List<Chamomile> getChamomiles() {
+        return Chamomiles;
+    }
+
+    public void setChamomiles(List<Chamomile> Chamomiles) {
+        this.Chamomiles=Chamomiles;
+    }
+
+    public List<Rose> getRoses () {
+        return Roses;
+    }
+
+    public void setRoses(List<Rose> Roses) {
+         this.Roses=Roses;
+
+    }
+
+    public List<Tulip> getTulips() {
+        return Tulips;
+    }
+
+    public void setTulips(List<Tulip> Tulips) {
+        this.Tulips=Tulips;
+    }
+
+
+
+}
+
+
+
+
---
+++ Rose_bush.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Rose_bush {
+    private List<Aster> Asters;
+
+    public List<Aster> Asters() {
+
+        return Asters;
+    }
+
+    public void Asters(List<Aster> Asters) {
+
+        this.Asters = Asters;
+    }
+
+    public void setAsters(List<Aster> Asterss) {
+        this.Asters = Asters;
+
+    }
+}
